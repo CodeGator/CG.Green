d9fa846d7e05511fd9472f67f981ddd4eb234f72
Martin Cook <33993749+martycook@users.noreply.github.com>
2023-01-21 16:07:59 -0600

    adding test projects

---
+++ tests/CG.Green.Host.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace CG.Green.Host.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Inconclusive("method not written yet");
+        }
+    }
+}
