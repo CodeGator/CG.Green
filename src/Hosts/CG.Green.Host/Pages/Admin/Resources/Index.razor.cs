50f6b4377abe06c29f5c00dbadcc835778454671
Martin Cook <33993749+martycook@users.noreply.github.com>
2023-01-31 07:37:30 -0600

    adding implementation

--- src/Hosts/CG.Green.Host/Pages/Admin/Resources/Index.razor.cs
+++ src/Hosts/CG.Green.Host/Pages/Admin/Resources/Index.razor.cs
@@ -5,5 +5,21 @@
 /// </summary>
 public partial class Index
 {
+    // *******************************************************************
+    // Fields.
+    // *******************************************************************
 
+    #region Fields
+
+    /// <summary>
+    /// This field contains a reference to breadcrumbs for the view.
+    /// </summary>
+    internal protected readonly List<BreadcrumbItem> _crumbs = new()
+    {
+        new BreadcrumbItem("Home", href: "/"),
+        new BreadcrumbItem("Admin", href: "/admin", disabled: true),
+        new BreadcrumbItem("Resources", href: "/admin/resources")
+    };
+
+    #endregion
 }
