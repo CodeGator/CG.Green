49aad8c18d530d5c2d11d4a2be49f543c0e7edd0
Martin Cook <33993749+martycook@users.noreply.github.com>
2023-02-05 14:12:47 -0600

    I added a test fixture for the apiscope manager

---
+++ tests/CG.Green.Tests/Managers/ApiScopeManagerFixture.cs
@@ -0,0 +1,91 @@
+﻿
+namespace CG.Green.Managers;
+
+/// <summary>
+/// This class is a test fixture for the <see cref="ApiScopeManager"/>
+/// class.
+/// </summary>
+[TestClass]
+public class ApiScopeManagerFixture
+{
+    // *******************************************************************
+    // Public methods.
+    // *******************************************************************
+
+    #region Public methods
+
+    /// <summary>
+    /// This method ensures the <see cref="ApiScopeManager.ApiScopeManager(Repositories.IApiScopeRepository, Microsoft.Extensions.Logging.ILogger{IApiScopeManager})"/>
+    /// constructor properly initializes object instances.
+    /// </summary>
+    [TestMethod]
+    public void ApiScopeManager_ctor()
+    {
+        // Arrange ...
+        var apiScopeRepository = new Mock<IApiScopeRepository>();
+        var logger = new Mock<ILogger<IApiScopeManager>>();
+
+        // Act ...
+        var result = new ApiScopeManager(
+            apiScopeRepository.Object,
+            logger.Object
+            );
+
+        // Assert ...
+        Assert.IsTrue(
+            result._apiScopeRepository == apiScopeRepository.Object,
+            "The _apiScopeRepository property wasn't initialized"
+            );
+        Assert.IsTrue(
+            result._logger == logger.Object,
+            "The _logger property wasn't initialized"
+            );
+
+        Mock.Verify(
+            apiScopeRepository,
+            logger
+            );
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This method ensures the <see cref="ApiScopeManager.AnyAsync(CancellationToken)"/>
+    /// method calls the proper repository method(s) and returns the correct
+    /// return value.
+    /// </summary>
+    [TestMethod]
+    public async Task ApiScopeManager_AnyAsync()
+    {
+        // Arrange ...
+        var apiScopeRepository = new Mock<IApiScopeRepository>();
+        var logger = new Mock<ILogger<IApiScopeManager>>();
+
+        apiScopeRepository.Setup(x => x.AnyAsync(
+            It.IsAny<CancellationToken>()
+            )).ReturnsAsync(true)
+            .Verifiable();
+
+        var manager = new ApiScopeManager(
+            apiScopeRepository.Object,
+            logger.Object
+            );
+
+        // Act ...
+        var result = await manager.AnyAsync()
+            .ConfigureAwait(false);
+
+        // Assert ...
+        Assert.IsTrue(
+            result,
+            "The return value was invalid"
+            );
+
+        Mock.Verify(
+            apiScopeRepository,
+            logger
+            );
+    }
+
+    #endregion
+}
