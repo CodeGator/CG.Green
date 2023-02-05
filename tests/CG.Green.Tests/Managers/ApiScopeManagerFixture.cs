c8ff47e39c0d3f6b1f24f81c382ab7c9f638e5d9
Martin Cook <33993749+martycook@users.noreply.github.com>
2023-02-05 14:30:21 -0600

    adding more tests

--- tests/CG.Green.Tests/Managers/ApiScopeManagerFixture.cs
+++ tests/CG.Green.Tests/Managers/ApiScopeManagerFixture.cs
@@ -1,4 +1,6 @@
 ﻿
+using Duende.IdentityServer.Models;
+
 namespace CG.Green.Managers;
 
 /// <summary>
@@ -78,6 +80,256 @@ public class ApiScopeManagerFixture
         // Assert ...
         Assert.IsTrue(
             result,
+            "The return value was invalid"
+            );
+
+        Mock.Verify(
+            apiScopeRepository,
+            logger
+            );
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This method ensures the <see cref="ApiScopeManager.CountAsync(CancellationToken)"/>
+    /// method calls the proper repository method(s) and returns the correct
+    /// return value.
+    /// </summary>
+    [TestMethod]
+    public async Task ApiScopeManager_CountAsync()
+    {
+        // Arrange ...
+        var apiScopeRepository = new Mock<IApiScopeRepository>();
+        var logger = new Mock<ILogger<IApiScopeManager>>();
+
+        apiScopeRepository.Setup(x => x.CountAsync(
+            It.IsAny<CancellationToken>()
+            )).ReturnsAsync(1)
+            .Verifiable();
+
+        var manager = new ApiScopeManager(
+            apiScopeRepository.Object,
+            logger.Object
+            );
+
+        // Act ...
+        var result = await manager.CountAsync()
+            .ConfigureAwait(false);
+
+        // Assert ...
+        Assert.IsTrue(
+            result == 1,
+            "The return value was invalid"
+            );
+
+        Mock.Verify(
+            apiScopeRepository,
+            logger
+            );
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This method ensures the <see cref="ApiScopeManager.CreateAsync(Duende.IdentityServer.Models.ApiScope, string, CancellationToken)"/>
+    /// method calls the proper repository method(s) and returns the correct
+    /// return value.
+    /// </summary>
+    [TestMethod]
+    public async Task ApiScopeManager_CreateAsync()
+    {
+        // Arrange ...
+        var apiScopeRepository = new Mock<IApiScopeRepository>();
+        var logger = new Mock<ILogger<IApiScopeManager>>();
+        var data = new Duende.IdentityServer.Models.ApiScope();
+
+        apiScopeRepository.Setup(x => x.CreateAsync(
+            It.IsAny<Duende.IdentityServer.Models.ApiScope>(),
+            It.IsAny<CancellationToken>()
+            )).ReturnsAsync(data)
+            .Verifiable();
+
+        var manager = new ApiScopeManager(
+            apiScopeRepository.Object,
+            logger.Object
+            );
+
+        // Act ...
+        var result = await manager.CreateAsync(
+            new Duende.IdentityServer.Models.ApiScope(),
+            "test"
+            ).ConfigureAwait(false);
+
+        // Assert ...
+        Assert.IsTrue(
+            result == data,
+            "The return value was invalid"
+            );
+
+        Mock.Verify(
+            apiScopeRepository,
+            logger
+            );
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This method ensures the <see cref="ApiScopeManager.DeleteAsync(Duende.IdentityServer.Models.ApiScope, string, CancellationToken)"/>
+    /// method calls the proper repository method(s) and returns the correct
+    /// return value.
+    /// </summary>
+    [TestMethod]
+    public async Task ApiScopeManager_DeleteAsync()
+    {
+        // Arrange ...
+        var apiScopeRepository = new Mock<IApiScopeRepository>();
+        var logger = new Mock<ILogger<IApiScopeManager>>();
+
+        apiScopeRepository.Setup(x => x.DeleteAsync(
+            It.IsAny<Duende.IdentityServer.Models.ApiScope>(),
+            It.IsAny<CancellationToken>()
+            )).Verifiable();
+
+        var manager = new ApiScopeManager(
+            apiScopeRepository.Object,
+            logger.Object
+            );
+
+        // Act ...
+        await manager.DeleteAsync(
+            new Duende.IdentityServer.Models.ApiScope(),
+            "test"
+            ).ConfigureAwait(false);
+
+        // Assert ...
+
+        Mock.Verify(
+            apiScopeRepository,
+            logger
+            );
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This method ensures the <see cref="ApiScopeManager.FindAllAsync(CancellationToken)"/>
+    /// method calls the proper repository method(s) and returns the correct
+    /// return value.
+    /// </summary>
+    [TestMethod]
+    public async Task ApiScopeManager_FindAllAsync()
+    {
+        // Arrange ...
+        var apiScopeRepository = new Mock<IApiScopeRepository>();
+        var logger = new Mock<ILogger<IApiScopeManager>>();
+        var data = Array.Empty<ApiScope>();
+
+        apiScopeRepository.Setup(x => x.FindAllAsync(
+            It.IsAny<CancellationToken>()
+            )).ReturnsAsync(data)
+            .Verifiable();
+
+        var manager = new ApiScopeManager(
+            apiScopeRepository.Object,
+            logger.Object
+            );
+
+        // Act ...
+        var result = await manager.FindAllAsync()
+            .ConfigureAwait(false);
+
+        // Assert ...
+        Assert.IsTrue(
+            result == data,
+            "The return value was invalid"
+            );
+
+        Mock.Verify(
+            apiScopeRepository,
+            logger
+            );
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This method ensures the <see cref="ApiScopeManager.FindByNameAsync(string, CancellationToken)"/>
+    /// method calls the proper repository method(s) and returns the correct
+    /// return value.
+    /// </summary>
+    [TestMethod]
+    public async Task ApiScopeManager_FindByNameAsync()
+    {
+        // Arrange ...
+        var apiScopeRepository = new Mock<IApiScopeRepository>();
+        var logger = new Mock<ILogger<IApiScopeManager>>();
+        var data = new ApiScope();
+
+        apiScopeRepository.Setup(x => x.FindByNameAsync(
+            It.IsAny<string>(),
+            It.IsAny<CancellationToken>()
+            )).ReturnsAsync(data)
+            .Verifiable();
+
+        var manager = new ApiScopeManager(
+            apiScopeRepository.Object,
+            logger.Object
+            );
+
+        // Act ...
+        var result = await manager.FindByNameAsync(
+            "test"
+            ).ConfigureAwait(false);
+
+        // Assert ...
+        Assert.IsTrue(
+            result == data,
+            "The return value was invalid"
+            );
+
+        Mock.Verify(
+            apiScopeRepository,
+            logger
+            );
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This method ensures the <see cref="ApiScopeManager.UpdateAsync(ApiScope, string, CancellationToken)"/>
+    /// method calls the proper repository method(s) and returns the correct
+    /// return value.
+    /// </summary>
+    [TestMethod]
+    public async Task ApiScopeManager_UpdateAsync()
+    {
+        // Arrange ...
+        var apiScopeRepository = new Mock<IApiScopeRepository>();
+        var logger = new Mock<ILogger<IApiScopeManager>>();
+        var data = new ApiScope();
+
+        apiScopeRepository.Setup(x => x.UpdateAsync(
+            It.IsAny<ApiScope>(),
+            It.IsAny<CancellationToken>()
+            )).ReturnsAsync(data)
+            .Verifiable();
+
+        var manager = new ApiScopeManager(
+            apiScopeRepository.Object,
+            logger.Object
+            );
+
+        // Act ...
+        var result = await manager.UpdateAsync(
+            new ApiScope(),
+            "test"
+            ).ConfigureAwait(false);
+
+        // Assert ...
+        Assert.IsTrue(
+            result == data,
             "The return value was invalid"
             );
 
